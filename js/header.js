9c5c89c29d4ce72387718ee0d49ceb97c05e3dbe
Celemm <ngolaceleste@gmail.com>
2025-11-27 23:32:10 +0100

    css

--- js/header.js
+++ js/header.js
@@ -5,7 +5,7 @@ const headerHTML = `
     <nav>
         <ul>
             <li><a href="index.html" class="nav-link">Accueil</a></li>
-                <li><a href="#profil" class="nav-link">Profil</a></li>
+                <li><a href="./profil.html" class="nav-link">Profil</a></li>
                 <li><a href="./contact.html" class="nav-link">Contact</a></li>
                 <li><a href="./projet.html" class="nav-link">Projets</a></li>
                 <li><a href="./veille.html" class="nav-link">Veille Techno</a></li>
